6e79c2c6a115288aa185cadfae2418ce227d5c55
Lenovo <jannatjui414@gmail.com>
2025-02-11 16:59:45 +0600

    complte contact page design

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,8 @@
 import contactImg from '../assets/contact.jpg'
-
+import mentor1 from '../assets/mentor-1.jpg'
+import mentor2 from '../assets/mentor-2.jpg'
+import mentor3 from '../assets/mentor-3.jpg'
+import mentor4 from '../assets/mentor-4.jpg'
 const Contact = () => {
     return (
         <>
@@ -26,16 +29,35 @@ const Contact = () => {
             {/* contat form */}
             <div className="hero bg-base-200 min-h-screen my-4">
                 <div className="flex max-w-[80rem] gap-[1rem] p-[1rem] align-top flex-col lg:flex-row-reverse">
-                    <div className=" lg:text-left">
-                        <h1 className=" text-3xl lg:text-5xl font-bold text-gray-700">Get in touch with us</h1>
+                    <div className=" lg:text-left lg: pt-10">
+                        <h1 className=" text-3xl lg:text-4xl font-bold text-gray-700">Get in touch with us</h1>
                         <p className="py-6 text-gray-700">
                         We believe in collaborative learning! If you have ideas, need assistance, or want to be a part of our growing study community, reach out today. Let’s learn, grow, and succeed together!
                         </p>
                         <h1 className=" text-2xl lg:text-4xl font-bold text-gray-700">Our Mentor</h1>
-                        <div></div>
+                        {/* mentor div */}
+                        <div className='grid grid-cols-2 lg:grid-cols-4 mt-4 gap-4 text-gray-700'>
+                        <div className='flex flex-col justify-center items-center'>
+                            <img className='w-20 h-20 rounded-full' src={mentor1} alt="menotr-1" />
+                            <p>mentor</p>
+                        </div>
+                        <div className='flex flex-col justify-center items-center'>
+                            <img className='w-20 h-20 rounded-full' src={mentor2} alt="menotr-1" />
+                            <p>mentor</p>
+                        </div>
+                        <div className='flex flex-col justify-center items-center'>
+                            <img className='w-20 h-20 rounded-full border-2' src={mentor3} alt="menotr-1" />
+                            <p>mentor</p>
+                        </div>
+                        <div className='flex flex-col justify-center items-center'>
+                            <img className='w-20 h-20 rounded-full' src={mentor4} alt="menotr-1" />
+                            <p>mentor</p>
+                        </div>
+                        </div>
                     </div>
                     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl rounded-lg">
-                        <form className="card-body">
+                        <form className="card-body w-full">
+                            <h1 className='text-xl font-bold text-gray-700'>Fill the form</h1>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Name</span>
@@ -62,7 +84,7 @@ const Contact = () => {
                             </div>
                             
                             <div className="form-control mt-6">
-                                <button className="btn btn-primary">Login</button>
+                                <button className="btn bg-[#0AB99D] text-white">Login</button>
                             </div>
                         </form>
                     </div>
