a47e71a3d872cc4f45e7b373fa02e566de4b44f6
Lenovo <jannatjui414@gmail.com>
2025-04-25 22:55:58 +0600

    costomised loading spinner

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../provider/AuthProvider';
 
 const Spinner = () => {
+     const { isDarkMode } = useContext(AuthContext)
     return (
         <div className='flex items-center'>
-           <span className="loading loading-spinner loading-lg"></span> 
+           <div className="flex flex-col items-center space-y-4">
+        <div className={`w-12 h-12 border-4 ${isDarkMode ? 'border-white ' : ' text-gray-800'}  border-dashed rounded-full animate-spin`}></div>
+        
+      </div>
         </div>
     );
 };
